ee61bb70af5803883321b05020f831a17303350a
akismkn <78148509+akismkn@users.noreply.github.com>
2022-11-25 21:54:02 +0200

    added 1 function. Now the game is 5 rounds.

--- script.js
+++ script.js
@@ -16,36 +16,62 @@ function roundStart(playerChoice,computerChoice){
     const plChoice = playerChoice.toLowerCase();
     const compChoice = computerChoice.toLowerCase();
     if (plChoice===compChoice){
-        return 'Draw!' + plChoice + 'ties' + compChoice;
+        return 'Draw! ' + plChoice + ' ties ' + compChoice;
     }
     if (plChoice==='rock'){
         if (compChoice==='paper'){
-            return 'You lose! Rock loses to Paper'
+            return 'Lose! Rock loses to Paper'
         }
         else{
-            return 'You win! Rock beats Scissors'
+            return 'Win! Rock beats Scissors'
         }
     }
     else if (plChoice==='paper'){
         if (compChoice==='rock'){
-            return 'You win! Paper beats Rock'
+            return 'Win! Paper beats Rock'
         }
         else{
-            return 'You lose! Paper loses to Scissors'
+            return 'Lose! Paper loses to Scissors'
         }
     }
     else{
         if(compChoice==='rock'){
-            return 'You lose! Scissors loses to Rock'
+            return 'Lose! Scissors loses to Rock'
         }
         else{
-            return 'You win! Scissors beats Paper'
+            return 'Win! Scissors beats Paper'
         }
     }
 }
 
-const playerChoice = prompt('Please enter Rock Paper Or Scissors :')
-const computerChoice = getComputerChoice();
-console.log(roundStart(playerChoice,computerChoice));
+function game(){
+    let playerCount=0;
+    let computerCount=0;
+    for (let i=1; i<=5; i++){
+        const playerChoice = prompt('Please enter Rock Paper Or Scissors :')
+        const computerChoice = getComputerChoice();
+        const result = roundStart(playerChoice,computerChoice);
+        if (result.charAt(0)==='W'){
+            playerCount++;
+            console.log('You won the round ' + i);
+        }
+        else if(result.charAt(0)==='L'){
+            computerCount++;
+            console.log('You lost the round ' + i);
+        }
+        else{
+            console.log('Draw at round ' + i);
+        }
+    }
+    if (playerCount>computerCount){
+        return 'You won! Player Score:' + playerCount + ' Computer Score:' + computerCount;
+    }
+    else if (playerCount<computerCount){
+        return 'You lost. Player Score:' + playerCount + ' Computer Score:' + computerCount;
+    }
+    else{
+        return 'Draw! Player Score:' + playerCount + ' Computer Score:' + computerCount;
+    }
+}
 
-
+console.log(game());
